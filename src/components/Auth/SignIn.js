f30f94ddbb4862752f56ff705f848dfd54e8e1fd
StéphaneCash <kikonistephane@gmail.com>
2023-03-21 20:11:16 +0100

    data

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -11,7 +11,7 @@ const SignIn = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState('');
-    const [tokenGoogle, setTokenGoogle] = useState("jkk");
+    const [tokenGoogle, setTokenGoogle] = useState("");
 
     const [btnClic, setBtnClic] = useState(false);
 
